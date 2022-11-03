5fbc483f5e8e94252a4046fdc7f0585595efa8f1
Nino Lambert <anthony.lamb001@gmail.com>
2022-11-03 11:18:40 -0400

    add EFS creation to the script. Need to implement waiting though.
    Co-authored-by: Eugene O'Fallon <epofallon@gmail.com>
    Co-authored-by: Aaron Hull <aahull08@gmail.com>
    Co-authored-by: Marcin Kostecki <49038365+marcinkostecki@users.noreply.github.com>

--- aws/README.md
+++ aws/README.md
@@ -2,4 +2,31 @@
 
 This script requires a few things.
 
-- a `.env` file with an `AWS_ACCESS_KEY` field and `AWS_SECRET_KEY` field. These credentials should correspond to an AWS user with the `AmazonECS_FullAccess` IAM policy applied to them.
+- a `.env` file with an `AWS_ACCESS_KEY` field and `AWS_SECRET_KEY` field. These credentials should correspond to an AWS user with the `AmazonECS_FullAccess` and the `AmazonElasticFileSystemFullAccess` IAM policies applied to them.
+
+Notes to self
+
+- Tried to create a task using an EFS volume programatically with no success. I got some errors about needing to configure my VPC to communicate with the EFS. Tried to create a task in the browser to do the same thing with the same result. Full error message is below.
+
+```
+ResourceInitializationError: failed to invoke EFS utils commands to set up EFS volumes: stderr: Failed to resolve "fs-0e4a39a0b7bb8b885.efs.us-east-1.amazonaws.com" - check that your file system ID is correct, and ensure that the VPC has an EFS mount target for this file system ID. See https://docs.aws.amazon.com/console/efs/mount-dns-name for more detail. Attempting to lookup mount target ip address using botocore. Failed to import necessary dependency botocore, please install botocore first. : unsuccessful EFS utils command execution; code: 1
+```
+
+Ok, looks like I need to mount something onto the EFS instance so that it can communicate with the outside world. Did that manually in the browser, lets see if the task I created works now... Nope, still getting that error... Odd, I made sure that the mount target is the same VPC and subnet that the task is running on... there must be something else I need to do to finish setting up the EFS.
+
+Oh, So that just creates the mount target. looks like I actually need to do something with that mount target in order for it to work.
+
+Tried running the task with a basic root dir in the volumes definition and got this error below.
+
+```
+ResourceInitializationError: failed to invoke EFS utils commands to set up EFS volumes: stderr: b'mount.nfs4: Connection timed out' : unsuccessful EFS utils command execution; code: 32
+```
+
+Got some help here: looks like I need to do some security group config:
+https://aws.amazon.com/premiumsupport/knowledge-center/fargate-unable-to-mount-efs/
+
+Looks like security groups are an EC2 feature. Also looks like the default one is set up to accept connections from anywhere, and that a new security group was created each time I executed a task. So THAT'S why this wasn't working: the default security group for EFS wasn't the same as the security group I was using to run the tasks with.
+
+I've got the default security group configured to accept connections from anywhere. So it looks like as long as I run the tasks with the default security group and have the EFS mount targets use the default security group, I should be good (right?)
+
+RIGHT!!! Looks like this works!
--- aws/index.js
+++ aws/index.js
@@ -1,16 +1,12 @@
-import {
-  ECS,
-  CreateCapacityProviderCommand,
-  waitForTasksRunning,
-  waitUntilTasksRunning,
-} from "@aws-sdk/client-ecs";
+import { ECS, waitUntilTasksRunning } from "@aws-sdk/client-ecs";
+import { EFS } from "@aws-sdk/client-efs";
 import dotenv from "dotenv";
 
 const waitForTask = async (taskName) => {
   let output = await waitUntilTasksRunning(
     {
       client: this,
-      maxWaitTime: 30, //task created from browser runs within 30 sec
+      maxWaitTime: 90, //task created from browser runs within 90 sec
     },
     {
       cluster: "bard-cluster",
@@ -20,62 +16,61 @@ const waitForTask = async (taskName) => {
   console.log("wait output", output);
 };
 
+const fileSystemIsReady = async (fileSystem, efs) => {
+  return new Promise((res, rej) => {
+    setTimeout(async () => {
+      console.log("checking file system status");
+      let output = await efs.describeFileSystems({
+        FileSystemId: fileSystem.FileSystemId,
+      });
+      console.log("describe file system output", output);
+      res(output);
+    }, 10 * 1000); //TODO: be more intelligent about this.
+  });
+};
+
 const main = async () => {
   dotenv.config();
-  const client = new ECS({
+  const ecs = new ECS({
     region: "us-east-1",
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_KEY,
     },
   });
-  client.waitForTask = waitForTask;
-  console.log("created a client");
-  //FARGATE and FARGATE_SPOT cap providers should be associated with the client
+  const efs = new EFS({
+    region: "us-east-1",
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    },
+  });
+  ecs.waitForTask = waitForTask;
+  console.log("created an ecs client");
+  //FARGATE and FARGATE_SPOT cap providers should be associated with the ecs client
   //if you want to use fargate:
   //https://docs.aws.amazon.com/AmazonECS/latest/developerguide/fargate-capacity-providers.html
-  const cluster = await client.createCluster({
+  const fileSystem = await efs.createFileSystem({});
+  console.log("created file system!", fileSystem);
+  const fileSystemStatus = await fileSystemIsReady(fileSystem, efs);
+  await efs.createMountTarget({
+    FileSystemId: fileSystem.FileSystemId,
+    SubnetId: "subnet-08e97a8a4d3098617", //TODO: How to get this?
+    SecurityGroups: ["sg-0824cc4158587a789"], //TODO: How to get this?
+  });
+  const cluster = await ecs.createCluster({
     capacityProviders: ["FARGATE", "FARGATE_SPOT"],
     clusterName: "bard-cluster",
   });
   console.log("created cluster");
-  const taskDef = await client.registerTaskDefinition({
+  const taskDef = await ecs.registerTaskDefinition({
     family: "postgres-task",
     //TODO: does this role exist?
     executionRoleArn: "ecsTaskExecutionRole",
     compatabilities: ["EC2", "FARGATE"],
     requiresCompatibilities: ["FARGATE"],
-    requiresAttributs: [
-      {
-        targetId: null,
-        targetType: null,
-        value: null,
-        name: "com.amazonaws.ecs.capability.logging-driver.awslogs",
-      },
-      {
-        targetId: null,
-        targetType: null,
-        value: null,
-        name: "ecs.capability.execution-role-awslogs",
-      },
-      {
-        targetId: null,
-        targetType: null,
-        value: null,
-        name: "ecs.capability.task-eni",
-      },
-    ],
     containerDefinitions: [
       {
-        logConfiguration: {
-          logDriver: "awslogs",
-          secretOptions: null,
-          options: {
-            "awslogs-group": "/ecs/test_pg",
-            "awslogs-region": "us-east-1",
-            "awslogs-stream-prefix": "ecs",
-          },
-        },
         image: "public.ecr.aws/docker/library/postgres:15-alpine",
         name: "postgres",
         //TODO: need a better value for this
@@ -93,6 +88,30 @@ const main = async () => {
           { name: "POSTGRES_USER", value: "user" },
           { name: "POSTGRES_PASSWORD", value: "password" },
         ],
+        mountPoints: [
+          {
+            sourceVolume: "initPg",
+            containerPath: "/docker-entrypoint-initdb.d",
+          },
+          {
+            sourceVolume: "persistPg",
+            containerPath: "/var/lib/postgresql/data",
+          },
+        ],
+      },
+    ],
+    volumes: [
+      {
+        name: "initPg",
+        efsVolumeConfiguration: {
+          fileSystemId: fileSystem.FileSystemId,
+        },
+      },
+      {
+        name: "persistPg",
+        efsVolumeConfiguration: {
+          fileSystemId: fileSystem.FileSystemId,
+        },
       },
     ],
     //these next pieces are all required by fargate
@@ -107,7 +126,7 @@ const main = async () => {
 
   //these also take some time to spin up once you run them... await doesn't
   //actually wait for the task to finish running. Need some other strat to do that.
-  let output = await client.runTask({
+  let output = await ecs.runTask({
     taskDefinition: "postgres-task",
     cluster: "bard-cluster",
     count: 1,
@@ -115,7 +134,7 @@ const main = async () => {
     //required by fargate: TODO. got these from the console. How can I get them programatically?
     networkConfiguration: {
       awsvpcConfiguration: {
-        subnets: ["subnet-08e97a8a4d3098617", "subnet-0cfa424c0f6ed7d89"],
+        subnets: ["subnet-08e97a8a4d3098617"],
         securityGroups: ["sg-0824cc4158587a789"],
         assignPublicIp: "ENABLED",
       },
@@ -124,7 +143,7 @@ const main = async () => {
   console.log("executed the postgres task");
   console.log("waiting for postgres task");
   try {
-    await client.waitForTask("postgres-task");
+    await ecs.waitForTask("postgres-task");
   } catch (error) {
     console.log("error waiting for postgres:", error);
   }
